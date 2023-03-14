cfb16c6955c038766ad2e4e9236c83def71e28d5
Caitlin Jordan <jordan.cait17@gmail.com>
2023-03-14 07:41:18 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  0(n)
 */
 
 /* 
-  Add your pseudocode here
+  create a loop to loop through the array two different ways
+  create if statement for if one integer + the other integer = target, return true
+  if nothing matches, return false
 */
 
 /*
-  Add written explanation of your solution here
+couldn't have else return true otherwise while we go through each integers to see if they
+add up to the target & they don't it would return false so the only way that would 
+return true is if every combination of the integers added up to the target
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,12 +31,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 1, 4, 2, 7], 5));
 
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 8, 8, 8], 4));
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
